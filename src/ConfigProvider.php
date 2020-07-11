bd571ed0feeeced8c3725a5a8806cf0620d67509
Andrey <alestro@bk.ru>
2020-07-12 00:24:43 +0200

    Create ConfigProvider.php

---
+++ src/ConfigProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Bermuda\RequestHandlerRunner;
+
+
+use Laminas\HttpHandlerRunner\RequestHandlerRunner;
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+
+
+final class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return ['dependencies' => [
+                'factories' => [
+                    EmitterInterface::class => EmitterFactory::class, 
+                    RequestHandlerRunner::class => RequestHandlerRunnerFactory::class'
+                ]
+            ]
+        ];
+    }
+}
