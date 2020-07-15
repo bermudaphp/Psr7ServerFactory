98059965257686928ccc4be7be1a3ad43114276f
Andrey <alestro@bk.ru>
2020-07-15 14:11:49 +0200

    Create ServerRequestFactory.php

---
+++ src/ServerRequestFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Bermuda\RequestHandlerRunner;
+
+
+use Bermuda\ErrorHandler\ErrorResponseGeneratorInterface;
+use Psr\Container\ContainerInterface;
+use Bermuda\Pipeline\PipelineInterface;
+use Nyholm\Psr7Server\ServerRequestCreator;
+use Psr\Http\Server\RequestHandlerInterface;
+use Laminas\HttpHandlerRunner\RequestHandlerRunner;
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+
+
+/**
+ * Class ServerRequestFactory
+ * @package Bermuda\RequestHandlerRunner
+ */
+final class ServerRequestFactory
+{
+    public function __invoke(ContainerInterface $c = null): ServerRequestInterface
+    {
+       return new RequestHandlerRunner($c->get(PipelineInterface::class), $this->getEmitter($c),
+            $this->getServerRequestFactory($c), static function(\Throwable $e) use ($c): ResponseInterface
+            {
+                return $c->get(ErrorResponseGeneratorInterface::class)->generate($e, (new ServerRequestFactory)());
+            }
+       );
+    }
+    
+    private function getEmitter(ContainerInterface $c): EmitterInterface
+    {
+        if($c->has(EmitterInterface::class))
+        {
+            return $c->get(EmitterInterface::class);
+        }
+        
+        return (new EmitterFactory)($c);
+    }
+    
+    private function getServerRequestFactory(ContainerInterface $c): callable
+    {
+        if($c->has('serverRequestFactory'))
+        {
+            return $c->get('serverRequestFactory');
+        }
+        
+        return new ServerRequestFactory;
+    }
+    
+    
+    private function getServerRequestCreator(): ServerRequestCreator
+    {
+        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+        return new ServerRequestCreator($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+    }
+}
