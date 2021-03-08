247ec63cbe4fb9d56464ef581909edb99f44274f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-03-08 18:02:13 +0300

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,10 +1,7 @@
 <?php
 
-
 namespace Bermuda\RequestHandlerRunner;
 
-
-use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriFactoryInterface;
 use Psr\Http\Message\RequestFactoryInterface;
@@ -14,28 +11,17 @@ use Psr\Http\Message\ServerRequestFactoryInterface;
 use Laminas\HttpHandlerRunner\RequestHandlerRunner;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 
-
-final class ConfigProvider
+final class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
-    public function __invoke(): array
+    protected function getFactories(): array
     {
-        return ['dependencies' => ['factories' => $this->getFactories()]];
-    }
-
-    private function getFactories(): array
-    {
-        $psr17factory = function()
-        {
-            return new Psr17Factory();
-        };
-
         return [
-            RequestFactoryInterface::class => $psr17factory,
-            UriFactoryInterface::class => $psr17factory,
-            ServerRequestFactoryInterface::class => $psr17factory,
-            ResponseFactoryInterface::class => $psr17factory,
-            UploadedFileFactoryInterface::class => $psr17factory,
-            StreamFactoryInterface::class => $psr17factory,
+            RequestFactoryInterface::class => Psr17Factory::class,
+            UriFactoryInterface::class => Psr17Factory::class,
+            ServerRequestFactoryInterface::class => Psr17Factory::class,
+            ResponseFactoryInterface::class => Psr17Factory::class,
+            UploadedFileFactoryInterface::class => Psr17Factory::class,
+            StreamFactoryInterface::class => Psr17Factory::class,
             EmitterInterface::class => EmitterFactory::class,
             RequestHandlerRunner::class => RequestHandlerRunnerFactory::class,
         ];
