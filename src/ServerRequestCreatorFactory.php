5da2dd8c9f580e31d61244119fbebde1363de7b2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-29 15:33:12 +0300

    Update ServerRequestCreatorFactory.php

--- src/ServerRequestCreatorFactory.php
+++ src/ServerRequestCreatorFactory.php
@@ -3,24 +3,24 @@
 namespace Bermuda\PSR7ServerFactory;
 
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Nyholm\Psr7Server\ServerRequestCreator;
-use Psr\Http\Message\UriFactory;
-use Psr\Http\Message\ServerRequestFactory;
-use Psr\Http\Message\UploadedFileFactory;
-use Psr\Http\Message\StreamFactory;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
 
 /**
  * Class ServerRequestCreatorFactory
- * @package Bermuda\RequestHandlerRunner
+ * @package Bermuda\PSR7ServerFactory
  */
 final class ServerRequestCreatorFactory
 {
     public function __invoke(ContainerInterface $container): ServerRequestCreator
     {
-        return new ServerRequestCreator($container->get(ServerRequestFactory::class), 
-            $container->get(UriFactory::class), $container->get(UploadedFileFactory::class),
-            $container->get(StreamFactory::class)
+        return new ServerRequestCreator($container->get(ServerRequestFactoryInterface::class),
+            $container->get(UriFactoryInterface::class), $container->get(UploadedFileFactoryInterface::class),
+            $container->get(StreamFactoryInterface::class)
         );
     }
 }
